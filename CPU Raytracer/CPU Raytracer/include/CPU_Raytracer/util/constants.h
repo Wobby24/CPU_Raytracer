8b2a7fe39e9714df9806dffd59e426677937d92c
Wobby24 <samuelbrinker26@ollwashmo.org>
2025-06-29 22:54:14 -0500

    Re-uploaded intial project code
    directories weren't setup correctly and required a restructure so it was cleaner

---
+++ CPU Raytracer/CPU Raytracer/include/CPU_Raytracer/util/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <limits>
+
+inline const double infinity = std::numeric_limits<double>::infinity();
+inline const double pi = 3.1415926535897932385;
+
+#endif // CONSTANTS_H
